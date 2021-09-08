36fb116fffae704851977fbd7302ccb07e503aed
ResminiLoris <resminiloris@gmail.com>
2021-09-08 16:41:14 +0200

    hotwell

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card"></div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .card{
+        height: 200px;
+        width: 200px;
+        background-color: #fff;
+        margin: 0;
+    }
+    
+</style>
